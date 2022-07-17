b54cf37008020833b4527ec03fe43be270afc8fd
Shubham07731 <89623700+Shubham07731@users.noreply.github.com>
2022-07-17 02:53:02 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector(".hamburger");
+
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+$(".cf_trigger").click(function (event) {
+  $(".cf_box").addClass("active");
+});
+$(".cf_close").click(function (event) {
+  $(".cf_box").removeClass("active");
+});
+$(".box").click(function (event) {
+  console.log("hello");
+  $(".project_info").addClass("project_active");
+});
+$(".project_close").click(function (event) {
+  console.log("close me");
+  $(".project_info").removeClass("project_active");
+});
